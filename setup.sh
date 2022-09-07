c2da1e91fa1c9c422b4edcb13fb6e87fe1edfe44
Shahid Aziz <theshahidaziz@gmail.com>
2022-09-07 14:51:19 +0200

    setup

--- setup.sh
+++ setup.sh
@@ -1,23 +1,27 @@
 mkdir -p ~/.streamlit/
+echo "[theme]
+# primaryColor = ‘#84a3a7’
+# backgroundColor = ‘#EFEDE8’
+# secondaryBackgroundColor = ‘#fafafa’
+# textColor= ‘#424242’
+# font = ‘sans serif’
+[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
 
 # echo "\
-# [general]\n\
-# email = \"${theshahidaziz@gmail.com}\"\n\
-# " > ~/.streamlit/credentials.toml
-
-
-
-echo "\
-[theme]\n\
-primaryColor='#E694FF'\n\
-backgroundColor='#00172B'\n\
-secondaryBackgroundColor="#0083BB"
-textColor='#FFFFFF'\n\
-font='sans serif'\n\
-[deprecation]\n\
-showPyplotGlobalUse = false\n\
-[server]\n\
-headless = true\n\
-enableCORS = false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+# [theme]\n\
+# primaryColor='#E694FF'\n\
+# backgroundColor='#00172B'\n\
+# secondaryBackgroundColor="#0083BB"
+# textColor='#FFFFFF'\n\
+# font='sans serif'\n\
+# [deprecation]\n\
+# showPyplotGlobalUse = false\n\
+# [server]\n\
+# headless = true\n\
+# enableCORS = false\n\
+# port = $PORT\n\
+# " > ~/.streamlit/config.toml
