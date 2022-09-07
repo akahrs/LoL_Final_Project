7df32d152fd23fda294c9fc882df185b337d58f9
Shahid Aziz <theshahidaziz@gmail.com>
2022-09-07 14:34:59 +0200

    app file

--- setup.sh
+++ setup.sh
@@ -1,11 +1,21 @@
 mkdir -p ~/.streamlit/
 
-echo "\
-[general]\n\
-email = \"${theshahidaziz@gmail.com}\"\n\
-" > ~/.streamlit/credentials.toml
+# echo "\
+# [general]\n\
+# email = \"${theshahidaziz@gmail.com}\"\n\
+# " > ~/.streamlit/credentials.toml
+
+
 
 echo "\
+[theme]\n\
+primaryColor='#E694FF'\n\
+backgroundColor='#00172B'\n\
+secondaryBackgroundColor="#0083BB"
+textColor='#FFFFFF'\n\
+font='sans serif'\n\
+[deprecation]\n\
+showPyplotGlobalUse = false\n\
 [server]\n\
 headless = true\n\
 enableCORS = false\n\
